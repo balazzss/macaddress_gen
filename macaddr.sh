eee0c37b23e35976855369bc3a763e483617e019
Balàzs <31547968+balazzss@users.noreply.github.com>
2022-04-09 14:21:42 +0200

    Update macaddr.sh

--- macaddr.sh
+++ macaddr.sh
@@ -7,7 +7,7 @@ MACADDR3="34:7d:b1:$(dd if=/dev/urandom bs=512 count=1 2>/dev/null | md5sum | se
 MACADDR4="34:7d:b1:54:$(dd if=/dev/urandom bs=512 count=1 2>/dev/null | md5sum | sed 's/^\(..\)\(..\).*$/\1:\2/')"
 
 echo "MAC entièrement àléatoires : $MACADDR0"
-echo "MAS avec 2 valeurs fixes   : $MACADDR1"
+echo "MAC avec 2 valeurs fixes   : $MACADDR1"
 echo "MAC avec 4 valeurs fixes   : $MACADDR2"
 echo "MAC avec 6 valeurs fixes   : $MACADDR3"
 echo "MAC avec 8 valeurs fixes   : $MACADDR4"
